b4cd2178ee374399988101822059dba0842434ac
Dimitri Valckenier <dimitri.valckenier@student.hogent.be>
2024-03-16 15:50:27 +0100

    Add logo to startscreen

--- app/src/main/java/com/example/biljart/MainActivity.kt
+++ app/src/main/java/com/example/biljart/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.biljart
 import android.os.Bundle // ktlint-disable import-ordering
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Brightness4
 import androidx.compose.material3.BottomAppBar
@@ -25,9 +27,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.example.biljart.ui.theme.BilliardTheme
 
 class MainActivity : ComponentActivity() {
@@ -92,10 +95,12 @@ fun ScaffoldStructure(toggleTheme: () -> Unit, appName: String) {
     ) { innerPadding -> // without innerPadding, the content will be placed at the top of the screen, so behind the top app bar
         Column(
             modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
+                .padding(innerPadding)
+                .fillMaxSize(), // fill the available space.
+            verticalArrangement = Arrangement.Center, // center the content vertically.
+            horizontalAlignment = Alignment.CenterHorizontally, // center the content horizontally.
         ) {
-            Text(
+            /*Text(
                 modifier = Modifier.padding(8.dp),
                 text =
                 """
@@ -105,7 +110,19 @@ fun ScaffoldStructure(toggleTheme: () -> Unit, appName: String) {
                 """.trimIndent(),
 //                color = MaterialTheme.colorScheme.primary,
                 style = MaterialTheme.typography.bodyMedium,
-            )
+            )*/
+
+            val logo = painterResource(R.drawable.logo8ball)
+            Surface {
+                // show the 8-ball logo
+                Image(
+                    painter = logo,
+                    contentDescription = "Logo",
+                    modifier = Modifier
+                        .fillMaxWidth() // uses  max available width
+                        .wrapContentHeight(align = Alignment.CenterVertically), // aligns the image vertically in the center
+                )
+            }
         }
     }
 }
