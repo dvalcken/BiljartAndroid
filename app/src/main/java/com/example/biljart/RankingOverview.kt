8ff081cb283993338699628925e93ce33f81f7f8
Dimitri Valckenier <dimitri.valckenier@student.hogent.be>
2024-03-17 11:56:47 +0100

    LazyColumn for list of ranks

--- app/src/main/java/com/example/biljart/RankingOverview.kt
+++ app/src/main/java/com/example/biljart/RankingOverview.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -24,20 +26,33 @@ fun RankingOverview(modifier: Modifier = Modifier) {
     val ranks = mutableStateOf(data.Rank.getAll())
 
     Box(modifier = modifier) {
-        Column {
-            for (item in ranks.value) {
+        LazyColumn {
+            items(ranks.value) {
                 Rank(
-                    player_id = item.player_id,
-                    name = item.name,
-                    rank = item.rank,
-                    total_frames_won = item.total_frames_won,
-                    total_frames_lost = item.total_frames_lost,
-                    total_matches_won = item.total_matches_won,
-                    total_matches_played = item.total_matches_played,
-                    modifier = Modifier.fillMaxWidth(),
+                    player_id = it.player_id,
+                    name = it.name,
+                    rank = it.rank,
+                    total_frames_won = it.total_frames_won,
+                    total_frames_lost = it.total_frames_lost,
+                    total_matches_won = it.total_matches_won,
+                    total_matches_played = it.total_matches_played,
                 )
             }
         }
+//        Column {
+//            for (item in ranks.value) {
+//                Rank(
+//                    player_id = item.player_id,
+//                    name = item.name,
+//                    rank = item.rank,
+//                    total_frames_won = item.total_frames_won,
+//                    total_frames_lost = item.total_frames_lost,
+//                    total_matches_won = item.total_matches_won,
+//                    total_matches_played = item.total_matches_played,
+//                    modifier = Modifier.fillMaxWidth(),
+//                )
+//            }
+//        }
     }
 }
 
