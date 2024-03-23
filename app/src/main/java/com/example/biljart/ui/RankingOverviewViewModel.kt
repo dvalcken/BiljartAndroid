d27230cbf0d3218564ef0d58a5ad4c47441e91fa
Dimitri Valckenier <dimitri.valckenier@student.hogent.be>
2024-03-23 17:21:18 +0100

    setup RankApiState WIP (1u14')

--- app/src/main/java/com/example/biljart/ui/RankingOverviewViewModel.kt
+++ app/src/main/java/com/example/biljart/ui/RankingOverviewViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.biljart.ui
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.biljart.data.RankSampler
@@ -14,6 +17,9 @@ class RankingOverviewViewModel : ViewModel() { // Lesson 4: 1:12:00
     private val _rankUiState = MutableStateFlow(RankingOverviewState(RankSampler.getAll())) // private mutable state flow to update the state
     val rankUiState: StateFlow<RankingOverviewState> = _rankUiState.asStateFlow() // craates a wrapper around the MutableStateFlow, expose the state as a flow, this is read-only (not mutable)
 
+    var rankApiState: RankApiState by mutableStateOf(RankApiState.Loading)
+        private set
+
     init {
         Log.i("RankingViewModel", "creating new instance $this")
         getApiRank()
@@ -21,8 +27,8 @@ class RankingOverviewViewModel : ViewModel() { // Lesson 4: 1:12:00
 
     private fun getApiRank() {
         viewModelScope.launch { // getRank is a suspend function, so we need to call it from a coroutine
-            val result = rankService.getRank()
-            println("RankingOverviewViewModel.getApiRank: $result")
+            val ranks = rankService.getRank()
+            println("RankingOverviewViewModel.getApiRank: $ranks")
         }
     }
 }
