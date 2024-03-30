2873cee0864a0066f5281f88814209af65b2469c
Dimitri Valckenier <dimitri.valckenier@student.hogent.be>
2024-03-30 21:57:47 +0100

    NoItemFoundComponent with image

--- app/src/main/java/com/example/biljart/ui/genericcomponents/NoItemFoundComponent.kt
+++ app/src/main/java/com/example/biljart/ui/genericcomponents/NoItemFoundComponent.kt
@@ -1,10 +1,28 @@
 package com.example.biljart.ui.genericcomponents
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import com.example.biljart.R
 
 @Composable
 fun NoItemFoundComponent(message: String) {
-    // TODO
-    Text(message)
+    Column(
+        modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.no_item_found),
+            contentDescription = "Error Icon",
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_small)),
+        )
+        Text(
+            text = message,
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small)),
+        )
+    }
 }
